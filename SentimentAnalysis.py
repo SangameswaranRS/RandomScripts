57b2009f23b158187da326c81d69e36995ee9490
sangameswaran <blackdrafteaglekrish@gmail.com>
2018-11-30 22:01:38 +0530

    Bug Fixes

--- SentimentAnalysis.py
+++ SentimentAnalysis.py
@@ -9,8 +9,8 @@ print('--Importing done--')
 
 # Nltk expects word features to be like this. ['Hello', 'World'] should be structured as {'Helo':True,'wordl':true}
 def create_features_from_words(words):
-    useful_words = [word for word in words if word not in stopwords.words("english")]
-    print('--stopped unused words--')
+    # useful_words = [word for word in words if word not in stopwords.words("english")]
+    # print('--stopped unused words--')
     return dict([(word,True) for word in words])
 
 # Get negative reviews from the corpus
@@ -28,13 +28,17 @@ def get_positive_reviews():
     for files in movie_reviews.fileids('pos'):
         wordsInFile = movie_reviews.words(files)
         totalPositiveReviews.append((create_features_from_words(wordsInFile),"Positive"))
-        return totalPositiveReviews
+    return totalPositiveReviews
 
 # Train the Classifier.
 def classifier_train_full():
     # Training the entire data. If score is needed, then split into training and testing datasets
     positiveReviews = get_positive_reviews()
     negativeReviews = get_negative_reviews()
+    print('----')
+    print(len(positiveReviews))
+    print(len(negativeReviews))
+    print('----')
     trainingData = positiveReviews[:800]+negativeReviews[:800]
     testingData = negativeReviews[800:]+ positiveReviews[800:]
     print('--Training Classifier--')
