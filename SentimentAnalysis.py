c251a1fd78122be9cc6453b7dfd80c1a97c35196
sangameswaran <blackdrafteaglekrish@gmail.com>
2018-11-30 12:34:34 +0530

    Some more

--- SentimentAnalysis.py
+++ SentimentAnalysis.py
@@ -1,14 +1,17 @@
-import nltk
-from nltk.corpus import movie_reviews
-from nltk.classify import NaiveBayesClassifier
-from nltk.tokenize import word_tokenize
+print('---Init---')
 import nltk.classify.util
+from nltk.classify import NaiveBayesClassifier
+from nltk.corpus import movie_reviews
 from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+print('--Importing done--')
 
 # Nltk expects word features to be like this. ['Hello', 'World'] should be structured as {'Helo':True,'wordl':true}
 def create_features_from_words(words):
     useful_words = [word for word in words if word not in stopwords.words("english")]
-    return dict([(word,True) for word in useful_words])
+    print('--stopped unused words--')
+    return dict([(word,True) for word in words])
 
 # Get negative reviews from the corpus
 def get_negative_reviews():
@@ -34,6 +37,7 @@ def classifier_train_full():
     negativeReviews = get_negative_reviews()
     trainingData = positiveReviews[:800]+negativeReviews[:800]
     testingData = negativeReviews[800:]+ positiveReviews[800:]
+    print('--Training Classifier--')
     classifier = NaiveBayesClassifier.train(trainingData)
     accuracy = nltk.classify.util.accuracy(classifier, testingData)
     print('---Model Accuracy='+str(accuracy)+'-------')
